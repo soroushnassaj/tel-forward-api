7a4e2453fc131b0ecc00a889eaddd7b0203f70a4
soroush <soroushnassaj@gmail.com>
2026-06-16 13:01:00 +0330

    Fix۶

--- api/index.py
+++ api/index.py
@@ -1,37 +1,52 @@
+import os
+import json
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-import os
+from http.server import BaseHTTPRequestHandler
 
 API_ID = int(os.environ["API_ID"])
 API_HASH = os.environ["API_HASH"]
 SESSION = os.environ["SESSION"]
 
-SOURCE_CHANNEL = "source_channel"
-DEST_CHAT = "target_chat"
+SOURCE = os.environ["SOURCE_CHANNEL"]
+DEST = os.environ["DESTINATION_CHAT"]
 
-async def handler(request):
 
-    client = TelegramClient(
-        StringSession(SESSION),
-        API_ID,
-        API_HASH
-    )
+class handler(BaseHTTPRequestHandler):
 
-    await client.connect()
+    def do_GET(self):
+        try:
+            result = self.run_forward()
 
-    messages = await client.get_messages(
-        SOURCE_CHANNEL,
-        limit=10
-    )
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
 
-    await client.forward_messages(
-        DEST_CHAT,
-        messages,
-        SOURCE_CHANNEL
-    )
+            self.wfile.write(json.dumps(result).encode())
 
-    await client.disconnect()
+        except Exception as e:
+            self.send_response(500)
+            self.end_headers()
+            self.wfile.write(str(e).encode())
 
-    return {
-        "status": "done"
-    }
+    def run_forward(self):
+
+        client = TelegramClient(
+            StringSession(SESSION),
+            API_ID,
+            API_HASH
+        )
+
+        with client:
+            messages = client.get_messages(SOURCE, limit=10)
+
+            client.forward_messages(
+                DEST,
+                messages,
+                SOURCE
+            )
+
+        return {
+            "status": "done",
+            "count": len(messages)
+        }
